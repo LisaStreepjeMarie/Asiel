43cef5a045800d39e3c16d6953c1880852af0278
barneveldlm <l.vanbarneveld@dictu.nl>
2020-05-15 11:18:31 +0200

    test

---
+++ src/main/java/asiel_project/APIApplication.java
@@ -0,0 +1,18 @@
+package asiel_project;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/api")
+public class APIApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> set = new HashSet<>();
+        set.add( DierResource.class );
+        set.add( VerblijfResource.class );
+        return set;
+    }
+}
